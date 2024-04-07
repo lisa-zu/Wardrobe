a19289ad215963b65644b5d29000bc2165ca036c
Roman <roman.zuchowski@gmail.com>
2024-04-07 12:04:54 +0200

    Initial Commit

---
+++ Wardrobe/WardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeApp.swift
+//  Wardrobe
+//
+//  Created by Roman Zuchowski on 07.04.24.
+//
+
+import SwiftUI
+
+@main
+struct WardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
