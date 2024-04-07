6a985ec54ef93077e8a7574df0bd658fb547dff2
Roman <roman.zuchowski@gmail.com>
2024-04-07 14:40:29 +0200

    adds files...

---
+++ Wardrobe/Features/Signup/SignupViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  SignupViewModel.swift
+//  Wardrobe
+//
+//  Created by Roman Zuchowski on 07.04.24.
+//
+
+import Foundation
+
+class SignupViewModel: ObservableObject {
+    @Published var signUpDisalbed: Bool = true
+    @Published var mailIsValid: Bool?
+}
+
+extension SignupViewModel {
+    func validateSignupForm(mail: String, password: String, passwordValid: String) {
+        
+    }
+    
+    func isValidEmail(_ email: String) {
+        // Regular expression pattern for email validation
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+
+        // Create NSPredicate with format matching email regex
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+
+        // Evaluate predicate with email string
+        DispatchQueue.main.async {
+            self.mailIsValid = emailPredicate.evaluate(with: email)
+        }
+    }
+}
