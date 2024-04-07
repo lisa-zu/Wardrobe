3bfcc5a637aa688360232517871afccc0a34713f
Roman <roman.zuchowski@gmail.com>
2024-04-07 14:40:40 +0200

    adds logout button for testing

--- Wardrobe/ContentView.swift
+++ Wardrobe/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var authService: AuthService
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button(action: {
+                authService.logoutEPAccount { error in
+                    debugPrint(error?.localizedDescription)
+                }
+            }, label: {
+                Text("LOGOUT")
+            })
         }
         .padding()
     }
