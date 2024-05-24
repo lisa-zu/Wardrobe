9025bb5e159d21e3553fc831a2f35d062e6ad145
Roman <lisa.lz.zuchowski@gmail.com>
2024-05-24 17:56:05 +0200

    adds new isFavorite parameter

--- Wardrobe/Models/WardrobeItem.swift
+++ Wardrobe/Models/WardrobeItem.swift
@@ -11,16 +11,18 @@ import SwiftData
 @Model
 final class WardrobeItem {
     var name: String
+    var isFavorite: Bool
     var category: WardrobeItemCategory
     var season: WardrobeItemSeason
     @Attribute(.externalStorage) var image: Data?
     var createdAt: Date
     
-    init(name: String, category: WardrobeItemCategory, season: WardrobeItemSeason, image: Data? = nil, createdAt: Date = .now) {
+    init(name: String, category: WardrobeItemCategory, season: WardrobeItemSeason, image: Data? = nil, createdAt: Date = .now, isFavorite: Bool = false) {
         self.name = name
         self.category = category
         self.season = season
         self.image = image
         self.createdAt = createdAt
+        self.isFavorite = isFavorite
     }
 }
