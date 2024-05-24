43be4d0c0a6c293ad7e9a3e8863992b0d626663c
Roman <lisa.lz.zuchowski@gmail.com>
2024-05-24 18:01:47 +0200

    this adds a new component view

---
+++ Wardrobe/Features/Outfit/WardrobeItemView.swift
@@ -0,0 +1,18 @@
+//
+//  WardrobeItemView.swift
+//  Wardrobe
+//
+//  Created by Roman Zuchowski on 24.05.24.
+//
+
+import SwiftUI
+
+struct WardrobeItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WardrobeItemView()
+}
