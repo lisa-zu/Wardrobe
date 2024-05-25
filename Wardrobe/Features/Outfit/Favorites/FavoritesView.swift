3307350c8f44d9c4fcea6963c82e8ac211dbc19a
Roman <lisa.lz.zuchowski@gmail.com>
2024-05-25 18:17:00 +0200

    this adds favoritesview and example images

---
+++ Wardrobe/Features/Outfit/Favorites/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Wardrobe
+//
+//  Created by Roman Zuchowski on 24.05.24.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
