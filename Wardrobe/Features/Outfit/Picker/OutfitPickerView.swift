58113e227ad034b148dbf190c27e98ecfa89d68d
Roman <lisa.lz.zuchowski@gmail.com>
2024-05-25 20:29:57 +0200

    this adds the filter sheets and buttons

--- Wardrobe/Features/Outfit/Picker/OutfitPickerView.swift
+++ Wardrobe/Features/Outfit/Picker/OutfitPickerView.swift
@@ -13,6 +13,9 @@ struct OutfitPickerView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [WardrobeItem]
     @State private var isShowingItemPickerView: Bool = false
+    @State private var isShowingFilterView: Bool = false
+    @State private var season: WardrobeItemSeason = .autumn
+    @State private var isActiveFilter: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -61,16 +64,29 @@ struct OutfitPickerView: View {
                 
             }
             .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
+                ToolbarItem(placement: .topBarLeading) {
                     Button {
                         self.isShowingItemPickerView.toggle()
                     } label: {
                         Image(systemName: "plus")
                     }
                 }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        self.isShowingFilterView.toggle()
+                    } label: {
+                        Image(systemName: isActiveFilter ? "line.3.horizontal.decrease.circle.fill" : "line.3.horizontal.decrease.circle")
+                    }
+                }
             }
             .sheet(isPresented: $isShowingItemPickerView) {
                 WardrobeItemPickerView()
+            }
+            .sheet(isPresented: $isShowingFilterView) {
+                WardrobeFiltersView(
+                    isActive: $isActiveFilter,
+                    season: $season
+                )
             }
         }
     }
