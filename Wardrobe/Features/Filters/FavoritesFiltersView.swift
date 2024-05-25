3f16005c593332de3303e85f5949426045f29ac6
Roman <lisa.lz.zuchowski@gmail.com>
2024-05-25 19:38:26 +0200

    this adds a favorites filters view

--- Wardrobe/Features/Filters/FavoritesFiltersView.swift
+++ Wardrobe/Features/Filters/FavoritesFiltersView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct FavoritesFiltersView: View {
+    
+    @Environment (\.dismiss) var dismiss
+    @Binding var season: WardrobeItemSeason
+    @Binding var category: WardrobeItemCategory
+    
+    init(season: Binding<WardrobeItemSeason>, category: Binding<WardrobeItemCategory>) {
+        self._season = season
+        self._category = category
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Picker("PICK_CATEGORY", selection: $category) {
+                    ForEach(WardrobeItemCategory.allCases) { _category in
+                        Text(LocalizedStringKey(stringLiteral: _category.rawValue))
+                    }
+                }
+                .pickerStyle(.navigationLink)
+                Picker("PICK_SEASON", selection: $season) {
+                    ForEach(WardrobeItemSeason.allCases) { _season in
+                        Text(LocalizedStringKey(stringLiteral: _season.rawValue))
+                    }
+                }
+                .pickerStyle(.navigationLink)
+            }
+            .navigationTitle("FILTER")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text(LocalizedStringKey(stringLiteral: "CLOSE"))
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    FavoritesFiltersView()
+    FavoritesFiltersView(season: .constant(.autumn), category: .constant(.dress))
 }
